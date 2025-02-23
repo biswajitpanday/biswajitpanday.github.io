3a62c88e9e56fb26687bb9c49868c5fe72b0b853
biswajitpanday <biswajitmailid@gmail.com>
2025-02-24 01:27:24 +0600

    Deployment fix 3

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  // trailingSlash: true,
+  distDir: 'out',
+  trailingSlash: true,
   basePath: "/biswajitpanday.github.io",
   // assetPrefix: "/biswajitpanday.github.io/",
   reactStrictMode: true,
